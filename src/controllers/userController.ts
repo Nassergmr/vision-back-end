2b7ac27abc61113ec3d883b1f9e61d91f854bb1a
Nasser <elhammouminasser99@gmail.com>
2025-08-26 02:07:55 +0100

    commit

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -442,6 +442,7 @@ export const GetAdminCollection = expressAsyncHandler(async (req, res) => {
         images: {
           include: { user: true },
           orderBy: { addedToCollection: "asc" },
+          where: { isVisible: true },
         },
       },
     });
